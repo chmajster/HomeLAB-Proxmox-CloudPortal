665ac5329b1469c796ef56d8df35f8433f040c94
chmajster <44300091+chmajster@users.noreply.github.com>
2026-08-25 14:19:10 +0200

    Load admin resource navigation enhancer

--- resources/views/layouts/app.php
+++ resources/views/layouts/app.php
@@ -25,6 +25,7 @@ $assetVersion = static function (string $path) use ($assetRoot): string {
     <script src="<?= htmlspecialchars((string) $basePath, ENT_QUOTES, 'UTF-8') ?>/assets/js/managed-provisioning.js?v=<?= $assetVersion('js/managed-provisioning.js') ?>" defer></script>
   <?php endif; ?>
   <script src="<?= htmlspecialchars((string) $basePath, ENT_QUOTES, 'UTF-8') ?>/assets/js/portal-enhancements.js?v=<?= $assetVersion('js/portal-enhancements.js') ?>" defer></script>
+  <script src="<?= htmlspecialchars((string) $basePath, ENT_QUOTES, 'UTF-8') ?>/assets/js/admin-resource-navigation.js?v=<?= $assetVersion('js/admin-resource-navigation.js') ?>" defer></script>
   <?php if ($page === 'vm-details'): ?>
     <script src="<?= htmlspecialchars((string) $basePath, ENT_QUOTES, 'UTF-8') ?>/assets/js/vm-details.js?v=<?= $assetVersion('js/vm-details.js') ?>" defer></script>
   <?php elseif ($page === 'project-details'): ?>
