a4d6d6a96d5690e91cf11caa8a11cc5b63cf7390
chmajster <44300091+chmajster@users.noreply.github.com>
2026-08-25 14:18:06 +0200

    Route admin resource details API

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 use CloudPortal\Application;
 use CloudPortal\Controllers\AdvancedVmController;
 use CloudPortal\Controllers\AdminController;
+use CloudPortal\Controllers\AdminResourceDetailsController;
 use CloudPortal\Controllers\AuthController;
 use CloudPortal\Controllers\CatalogController;
 use CloudPortal\Controllers\DashboardController;
@@ -32,6 +33,7 @@ return static function (Router $router, Application $app): void {
     $catalog = new CatalogController($app);
     $resources = new ResourceController($app);
     $admin = new AdminController($app);
+    $adminDetails = new AdminResourceDetailsController($app);
     $projectAdmin = new ProjectAdminController($app);
     $projectCreate = new ProjectCreateController($app);
     $proxmoxAdmin = new ProxmoxAdminController($app);
@@ -89,6 +91,7 @@ return static function (Router $router, Application $app): void {
     $router->add('POST', '/api/v1/admin/quotas', [$quotaAdmin, 'upsert']);
     $router->add('POST', '/api/v1/admin/quota-template-limits', [$quotaAdmin, 'upsertTemplateLimit']);
     $router->add('DELETE', '/api/v1/admin/quota-template-limits/{id}', [$quotaAdmin, 'deleteTemplateLimit']);
+    $router->add('GET', '/api/v1/admin/details/{resource}/{id}', [$adminDetails, 'show']);
     $router->add('GET', '/api/v1/admin/{resource}', [$admin, 'index']);
     $router->add('GET', '/api/v1/admin/networks/discovery', [$proxmoxAdmin, 'networkDiscovery']);
     $router->add('GET', '/api/v1/admin/storages/discovery', [$proxmoxAdmin, 'storageDiscovery']);
